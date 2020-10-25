a4e240691bf15547e13d1ef5eff9146fd3e783aa
hpbonfim <hp_bonfim@hotmail.com>
2020-10-25 20:52:57 -0300

    test postgres

--- banco-de-dados/conexao.php
+++ banco-de-dados/conexao.php
@@ -1,6 +1,13 @@
 <?php
 
-$conn = pg_connect(getenv("postgres://pjkzxaixobtarh:14fa59ec81efe1f415dc9b89c1c3c4f392c1e4351c1959d98e34d2dd7bfaffdc@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d54ikg6rb6ffr")) or die('Conexão com banco de dados falhou');
+$servername = "ec2-34-192-122-0.compute-1.amazonaws.com";
+$username = "pjkzxaixobtarh";
+$password = "14fa59ec81efe1f415dc9b89c1c3c4f392c1e4351c1959d98e34d2dd7bfaffdc";
+$database = "d54ikg6rb6ffr";
+
+$con_string = "host=" . $servername . "port=5432 dbname=" . $database . " user=" . $username . " password=" . $password . "";
+
+$conn = pg_connect($con_string) or die('Conexão com banco de dados falhou');
 
 
 /* ---- */
