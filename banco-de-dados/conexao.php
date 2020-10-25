9f41367533d8ff2da6a20c96e01a3a9d1478912f
hpbonfim <hp_bonfim@hotmail.com>
2020-10-25 20:36:06 -0300

    test postgres

--- banco-de-dados/conexao.php
+++ banco-de-dados/conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = pg_connect(getenv("postgres://pjkzxaixobtarh:14fa59ec81efe1f415dc9b89c1c3c4f392c1e4351c1959d98e34d2dd7bfaffdc@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d54ikg6rb6ffr"));
+$conn = pg_connect(getenv("postgres://pjkzxaixobtarh:14fa59ec81efe1f415dc9b89c1c3c4f392c1e4351c1959d98e34d2dd7bfaffdc@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d54ikg6rb6ffr")) or die('Conexão com banco de dados falhou');
 
 
 /* ---- */
@@ -12,8 +12,8 @@ $conn = pg_connect(getenv("postgres://pjkzxaixobtarh:14fa59ec81efe1f415dc9b89c1c
 
     // Criando conexão
     $conn = mysqli_connect($servername, $username, $password, $database);
-*/
-// Verificando a conexão
-if (!$conn) {
+
+if (!$conn) { // Verificando a conexão
     die("A conexão com o Banco de dados falhou: " . mysqli_connect_error());
 }
+*/
