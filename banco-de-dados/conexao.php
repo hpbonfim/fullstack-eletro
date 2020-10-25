410f039f673cd7f9b20bfa2e8d0da8fbe89d61cb
hpbonfim <hp_bonfim@hotmail.com>
2020-10-25 20:26:58 -0300

    test postgres

--- banco-de-dados/conexao.php
+++ banco-de-dados/conexao.php
@@ -1,15 +1,13 @@
-<?php   
-    $url = parse_url(getenv("postgres://pjkzxaixobtarh:14fa59ec81efe1f415dc9b89c1c3c4f392c1e4351c1959d98e34d2dd7bfaffdc@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d54ikg6rb6ffr"));
+<?php
 
+$server = "ec2-34-192-122-0.compute-1.amazonaws.com";
+$user = "pjkzxaixobtarh";
+$pass = "14fa59ec81efe1f415dc9b89c1c3c4f392c1e4351c1959d98e34d2dd7bfaffdc";
+$db = "d54ikg6rb6ffr";
 
-    $server = $url["host"];
-    $user = $url["user"];
-    $pass = $url["pass"];
-    $db = substr($url["path"], 1);
+$conn = pg_connect($server, $user, $pass, $db);
 
-    $conn = pg_connect($server, $user, $pass, $db);
-
-    /* ---- */
+/* ---- */
 /*
     $servername = "mysql";
     $username = "root";
@@ -19,7 +17,7 @@
     // Criando conexão
     $conn = mysqli_connect($servername, $username, $password, $database);
 */
-    // Verificando a conexão
-    if(!$conn) {
-        die("A conexão com o Banco de dados falhou: " . mysqli_connect_error());
-    }
+// Verificando a conexão
+if (!$conn) {
+    die("A conexão com o Banco de dados falhou: " . mysqli_connect_error());
+}
